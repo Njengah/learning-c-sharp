b933762d5af65bc7dfc99c3c08487ba832657b08
Njengah <joe@njengah.com>
2020-04-07 01:15:01 +0300

    added oop interface code snippet working example

---
+++ oop-interface-learn.cs
@@ -0,0 +1,62 @@
+using System;
+
+
+//Creating interface class which is a completely abstract class that does not have the normal methods only abstract methods 
+					
+public class Rectangle
+{
+	
+	//interface
+	
+	interface RectInterface{
+	
+	  int Length(); 
+	  int Width(); 
+	
+	}
+	
+	class RectDimensions:RectInterface{
+	
+		public int Length(){
+			
+			return 50; 
+			
+		}
+
+		public int Width(){
+			 
+			return 40; 
+
+		}
+		
+		
+	}
+	
+	
+	class Area : RectDimensions{
+	
+	  public int calcArea(){
+	  
+	    return  Length() * Width(); 
+	  
+	   }
+	
+	
+	}
+	
+	//Main method 
+	
+	public static void Main(){
+		
+		//Console.WriteLine("Joe Testing Interface here :) ");
+		
+		//create Object 
+		
+		Area AreaValue = new Area(); 
+		
+		Console.WriteLine(AreaValue.calcArea());  
+		
+	}
+	
+	
+}
